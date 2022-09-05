ccffdffa4b8bac87314e62608fa95e3160e68b10
S Gupta <naveen-kumar.s-gupta@capgemini.com>
2022-09-05 17:57:20 +0530

    initial release

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ echo 'Installing dependencies'
 yarn
 
 echo 'Deploying Insfrastructure in your AWS account'
-yarn cdk bootstrap
-yarn cdk deploy --require-approval never
+yarn cdk --profile mlops bootstrap --cloudformation-execution-policies arn:aws:iam::525419040953:policy/MlOps aws://525419040953/ap-south-1
+yarn cdk --profile mlops deploy --require-approval never
 
 popd
